d44ae25dae7e7e926a3a432967fe14f040929ca8
Galvin Adriel Darvell <63958470+glvn5150@users.noreply.github.com>
2026-05-07 15:39:45 +0700

    Update _v_dem.R

--- correlates_of_war_and_IPE/_v_dem.R
+++ correlates_of_war_and_IPE/_v_dem.R
@@ -1,10 +1,44 @@
+library(dplyr)
+library(tidyverse)
 
 v_dem <- readRDS("V-Dem-CY-Core-v16.rds")
 unique(v_dem$country_id)
 unique(v_dem$country_name)
 
+v_dem_indo <- filter(v_dem, v_dem$country_name == "Indonesia")
+v_dem_indo_clean <- filter(v_dem_indo, v_dem_indo$year > '1944')
+View(v_dem_indo_clean)
 
-indo <- filter(v_dem, v_dem$country_name == "Indonesia")
-ts.plot(indo$v2x_polyarchy_sd)
-ts.plot(indo$v2edplural)
-ts.plot(indo$v2x_libdem)
+
+#polyarch : To what extent is the ideal of electoral democracy in its fullest sense achieved?()
+#--->"Closed Autocratic", 0.25 as "Autocratic", 0.5 as"Ambivalent", 0.75 as "Minimally Democratic", and 1.0 as "Democratic"##
+#partidem : To what extent is the ideal of participatory democracy achieved?
+#--->0.0 as "Closed Autocratic", 0.25 as "Autocratic", 0.5 as"Ambivalent", 0.75 as "Minimally Democratic", and 1.0 as "Democratic".
+#mpi : To what extent is the electoral principle of democracy achieved?
+#---> 0.0 as "Closed Autocratic", 0.25 as "Autocratic", 0.5 as"Ambivalent", 0.75 as "Minimally Democratic", and 1.0 as "Democratic".
+
+polyarch <- v_dem_indo_clean$v2x_polyarchy
+partidem <- v_dem_indo_clean$v2x_partipdem
+mpi <- v_dem_indo_clean$v2x_mpi$
+text(polyarch, partidem, labels = v_dem_indo_clean$year, pos = 3, cex = 0.7)
+
+year_labels <- v_dem_indo_clean$year
+year_labels[as.numeric(year_labels) %% 5 != 0] <- "" 
+text(polyarch, partidem, labels = year_labels, pos = 3, cex = 0.7)
+
+plot(polyarch, partidem, main = "Indonesia: Electoral vs Participatory Democracy", # The Titlepch = 21,       
+     bg = "skyblue",   
+     col = "blue",     
+     cex = 1.2) 
+text(polyarch, partidem, labels = v_dem_indo_clean$year, pos = 3, cex = 0.7)
+abline(reg_poly_parti, col = "red", lwd = 2)
+
+  
+length(polyarch)
+length(partidem)
+plot(polyarch, partidem)
+abline(reg_poly_parti, col = "red", lwd = 2)
+
+
+reg_poly_parti <- lm(partidem ~ polyarch)
+summary(reg_poly_parti)
